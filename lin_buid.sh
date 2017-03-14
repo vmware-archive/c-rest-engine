287561f2cd004f601a946e67414467d0c47abea7
Danut Moraru <dmoraru@vmware.com>
2017-03-13 19:32:28 -0700

    Fix Linux build script for changed config file names, avoid duplicate iptables rules

--- lin_buid.sh
+++ lin_buid.sh
@@ -15,25 +15,26 @@ pushd build
 make -f Makefile.bootstrap DEBUG=1
 if [ $? -eq 0 ]; then
     if [ $install = true ]; then
-	rpm -e vmware-rest
-        port=$(cat ../docs/SampleRestEngineConfig.cfg | grep Port | cut -d' ' -f2)
+        rpm -e vmware-rest
+        port=$(cat ../docs/SampleRestEngineConfigPlain.cfg | grep Port | cut -d' ' -f2 | cut -d'p' -f1 | cut -d'P' -f1)
         version=$(cat package/rpm/vmware-rest.spec | grep Version | cut -d' ' -f2)
-	release=$(cat package/rpm/vmware-rest.spec | grep Release | cut -d' ' -f2)
-	pushd rpmbuild/RPMS/x86_64
-	rpm -ivh vmware-rest-$version-$release.x86_64.rpm
-	popd
-        iptables -A INPUT -p tcp --dport $port -j ACCEPT
-	iptables -A OUTPUT -p tcp --dport $port -j ACCEPT
-	cp ../docs/SampleRestEngineConfig.cfg /tmp/restconfig.txt
-	cp ../docs/RestEngineStart.tcf /opt/vmware/sbin/
-	cp ../docs/RestEngineStop.tcf /opt/vmware/sbin/
-	cp ../docs/RestStartStopRepeat.tcf /opt/vmware/sbin/
+        release=$(cat package/rpm/vmware-rest.spec | grep Release | cut -d' ' -f2)
+        pushd rpmbuild/RPMS/x86_64
+        rpm -ivh vmware-rest-$version-$release.x86_64.rpm
+        popd
+        iptables -C INPUT -p tcp --dport $port -j ACCEPT
+        if [ $? -ne 0 ]; then
+            iptables -A INPUT -p tcp --dport $port -j ACCEPT
+            iptables -A OUTPUT -p tcp --dport $port -j ACCEPT
+        fi
+        cp ../docs/SampleRestEngineConfigPlain.cfg /tmp/restconfig.txt
+        cp ../docs/SampleRestEngineConfigPlain.cfg /root/restconfig.txt
     fi
 else 
     if [ $install = true ]; then
-	echo "Build failed!"
+        echo "Build failed!"
     else 
-	echo "Build failed, abandoning installation!"
+        echo "Build failed, abandoning installation!"
     fi
 fi
 popd
